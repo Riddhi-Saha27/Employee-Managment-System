c94038f7be2a5546b1cfd6562d8e85d5ac7b2087
Riddhi Saha <114898762+Riddhi-Saha27@users.noreply.github.com>
2024-10-02 14:31:42 -0400

    Add files via upload

---
+++ Script-11.sql
@@ -0,0 +1,62 @@
+ALTER TABLE employees
+ADD SSN INT NULL;
+
+UPDATE employees
+SET SSN = 123120001
+WHERE empid = 1;
+
+UPDATE employees 
+SET SSN = 123120002
+WHERE empid = 2;
+
+UPDATE employees
+SET SSN = 123120003
+WHERE empid = 3;
+
+UPDATE employees
+SET SSN = 123120004
+WHERE empid = 4;
+
+UPDATE employees
+SET SSN = 123120005
+WHERE empid = 5;
+
+UPDATE employees
+SET SSN = 123120006
+WHERE empid = 6;
+
+UPDATE employees
+SET SSN = 123120007
+WHERE empid = 7;
+
+UPDATE employees
+SET SSN = 123120008
+WHERE empid = 8;
+
+UPDATE employees
+SET SSN = 123120009
+WHERE empid = 9;
+
+UPDATE employees
+SET SSN = 123120010
+WHERE empid = 10;
+
+UPDATE employees
+SET SSN = 123120011
+WHERE empid = 11;
+
+UPDATE employees
+SET SSN = 123120012
+WHERE empid = 12;
+
+UPDATE employees
+SET SSN = 123120013
+WHERE empid = 13;
+
+UPDATE employees
+SET SSN = 123120014
+WHERE empid = 14;
+
+UPDATE employees
+SET SSN = 123120015
+WHERE empid = 15;
